3ba5169f0f37bd22e1d14c54dc010fb516f486e5
Enzo Sorano <99842175+enzoSorano@users.noreply.github.com>
2025-03-06 09:14:03 -0600

    Adding functionality to Egg Timer
    learning if/else, switch statements, dictionaries, and optionals

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes = ["Soft": 3, "Medium": 5, "Hard": 10]
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var timer = Timer()
+   
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        print("test")
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime
+        {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        }
+    
+    }
+    
+    
 
 }
